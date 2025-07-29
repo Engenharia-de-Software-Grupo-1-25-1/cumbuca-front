047581701c398362fea368fd84274692c1051d53
lucsa-a <lucas.andre.monteiro.sousa@ccc.ufcg.edu.br>
2025-07-28 22:03:38 -0300

    feat: preparação da tela 3

---
+++ src/pages/Tela3.jsx
@@ -0,0 +1,13 @@
+import Header from '../components/Header2';
+import '../styles/Tela3.css';
+
+//Layout Padrão com Header simples
+function Tela3() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default Tela3;
