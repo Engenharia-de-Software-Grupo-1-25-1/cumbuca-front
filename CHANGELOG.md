07f693c43ce28506bb68adc65b1020eaf5834deb
ViniciusI4n <vinicius.ian.alves.lopes@ccc.ufcg.edu.br>
2025-08-02 15:38:33 -0300

    chore: ajustes de tamanho de texto e atualizacao do changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# CHANGELOG
+
+## US4.2 - PR 4 
+### Adicionando
+- Criação da tela de login e estruturação inicial de autenticação
--- src/components/Header1.jsx
+++ src/components/Header1.jsx
@@ -4,7 +4,7 @@ import bandeiras from '../assets/bandeiras.svg';
 function Header() {
   return (
     <>
-      <div className="w-full h-auto max-h-32 sm:max-h-24 lg:max-h-40 object-cover">
+      <div className="w-full h-auto max-h-32 sm:max-h-24 lg:max-h-38 object-cover">
         <img src={bandeiras} alt="Bandeiras" className="w-full object-cover" style={{ marginTop: '1.5rem' }} />
       </div>
 
@@ -16,7 +16,7 @@ function Header() {
             className="logo h-20 sm:h-24 md:h-28 lg:h-32 w-auto mt-[-1rem] sm:mt-[-1.5rem] md:mt-[-4rem] mb-4"
           />
         </a>
-        <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-extrabold tracking-wide mb-7">CUMBUCA</h1>
+        <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-5xl font-extrabold tracking-wide mb-7">CUMBUCA</h1>
       </div>
     </>
   );
