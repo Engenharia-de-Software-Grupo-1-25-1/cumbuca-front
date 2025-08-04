407a9b6e833456f63b812c6286d30c243eb0413e
ArthurCoelho <arthur.sousa.coelho@ccc.ufcg.edu.br>
2025-08-03 23:02:15 -0300

    feat: adiciona verificacao estatica

--- src/pages/RecuperarSenha.jsx
+++ src/pages/RecuperarSenha.jsx
@@ -1,9 +1,22 @@
-import React from "react";
-import Header from "../components/Header1";
-import "../styles/RecuperarSenha.css";
-import { FaEnvelope } from "react-icons/fa";
+import React, { useState } from 'react';
+import Header from '../components/Header1';
+import '../styles/RecuperarSenha.css';
+import { FaEnvelope, FaExclamationCircle } from 'react-icons/fa';
 
-function RecuperarSenha() {
+const RecuperarSenha = () => {
+  const [email, setEmail] = useState('');
+  const [error, setError] = useState(false);
+
+  const handleButtonClick = () => {
+    const emailMockado = 'tuquinha@gmail.com';
+    if (email !== emailMockado) {
+      setError(true);
+    } else {
+      setError(false);
+      // seguir o fluxo
+    }
+  };
+
   return (
     <div className="recuperar-wrapper">
       <Header />
@@ -13,15 +26,31 @@ function RecuperarSenha() {
           Informe seu e-mail cadastrado para redefinir sua senha
         </p>
 
-        <div className="input-group">
+        <div className={`input-group`}>
           <FaEnvelope className="icon" />
-          <input type="email" placeholder="E-mail" className="email-input" />
+          <div className="input-area">
+            <input
+              type="email"
+              placeholder="E-mail"
+              className="email-input"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+            {error && (
+              <div className="erro-msg">
+                <FaExclamationCircle className="erro-icon" />
+                <span>E-mail não encontrado</span>
+              </div>
+            )}
+          </div>
         </div>
 
-        <button className="btn-enviar">Enviar</button>
+        <button className="btn-enviar" onClick={handleButtonClick}>
+          Enviar
+        </button>
       </div>
     </div>
   );
-}
+};
 
 export default RecuperarSenha;
