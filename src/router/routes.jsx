87954937b6cd45db8476f00a50cc607cc8b3c174
ArthurCoelho <arthur.sousa.coelho@ccc.ufcg.edu.br>
2025-08-11 22:35:40 -0300

    resolve conflitos

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -4,12 +4,9 @@ import NaoEncontrado from '../pages/NaoEncontrado';
 import PrivateRoute from './PrivateRoute';
 import PublicRoute from './PublicRoute';
 import Layout4 from '../components/layouts/Layout4';
-<<<<<<< HEAD
 import RecuperarSenha from '../pages/RecuperarSenha';
 import NovaSenha from '../pages/NovaSenha';
-=======
 import CriarConta from '../pages/CriarConta';
->>>>>>> 14043ae2ad67f4923340cfb18e2b7b1a4a5bd692
 
 export default function RoutesApp() {
   return (
@@ -32,12 +29,9 @@ export default function RoutesApp() {
         }
       />
       <Route path="*" element={<NaoEncontrado />} />
-<<<<<<< HEAD
       <Route path="/recuperar-senha" element={<RecuperarSenha />} />
       <Route path="/alterar-senha" element={<NovaSenha />} />
-=======
       <Route path="/cadastrar-usuario" element={<CriarConta />} />
->>>>>>> 14043ae2ad67f4923340cfb18e2b7b1a4a5bd692
     </Routes>
   );
 }
