87ff166b4816096f81be1a2945ee6730efe490b5
ViniciusI4n <vinicius.ian.alves.lopes@ccc.ufcg.edu.br>
2025-08-11 22:40:20 -0300

    fix: corrige lint

--- src/components/ModalAvaliacao.jsx
+++ src/components/ModalAvaliacao.jsx
@@ -148,6 +148,36 @@ function TagInput({ tags, setTags, placeholder = 'Digite e pressione espaço' })
   );
 }
 
+const UF_BY_STATE = {
+  Acre: 'AC',
+  Alagoas: 'AL',
+  Amapá: 'AP',
+  Amazonas: 'AM',
+  Bahia: 'BA',
+  Ceará: 'CE',
+  'Distrito Federal': 'DF',
+  'Espírito Santo': 'ES',
+  Goiás: 'GO',
+  Maranhão: 'MA',
+  'Mato Grosso': 'MT',
+  'Mato Grosso do Sul': 'MS',
+  'Minas Gerais': 'MG',
+  Pará: 'PA',
+  Paraíba: 'PB',
+  Paraná: 'PR',
+  Pernambuco: 'PE',
+  Piauí: 'PI',
+  'Rio de Janeiro': 'RJ',
+  'Rio Grande do Norte': 'RN',
+  'Rio Grande do Sul': 'RS',
+  Rondônia: 'RO',
+  Roraima: 'RR',
+  'Santa Catarina': 'SC',
+  'São Paulo': 'SP',
+  Sergipe: 'SE',
+  Tocantins: 'TO',
+};
+
 function getUF(addr = {}) {
   if (addr.state_code && /^[A-Z]{2}$/.test(addr.state_code)) return addr.state_code;
 
