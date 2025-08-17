b0fb7be3cdce19f302f342babb44196fbe15ae38
ViniciusI4n <vinicius.ian.alves.lopes@ccc.ufcg.edu.br>
2025-08-17 12:28:55 -0300

    chore: ajustes de seguranca de login e get de user logado

--- src/router/PublicRoute.jsx
+++ src/router/PublicRoute.jsx
@@ -1,6 +1,6 @@
 import { Navigate } from 'react-router-dom';
 
 export default function PublicRoute({ children }) {
-  const token = localStorage.getItem('token');
+  const token = document.cookie.match(/(^| )auth_token=([^;]+)/);
   return token ? <Navigate to="/feed" replace /> : children;
 }
