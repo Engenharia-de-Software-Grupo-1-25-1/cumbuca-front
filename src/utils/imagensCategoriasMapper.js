309cf84d3b96907df7acb7c9bba7e93bb55a61a1
KaioCar64 <kaio.silva.carvalho@ccc.ufcg.edu.br>
2025-08-17 20:34:45 -0300

    feat: Adicionando mais imagens de estabelecimentos

--- src/utils/imagensCategoriasMapper.js
+++ src/utils/imagensCategoriasMapper.js
@@ -4,6 +4,15 @@ import cafe from '../assets/estabImagens/cafeteria.png';
 import bar from '../assets/estabImagens/bar.png';
 import pizzaria from '../assets/estabImagens/pizzaria.png';
 import cumbuca from '../assets/estabImagens/cumbuca.png';
+import sorveteria from '../assets/estabImagens/sorveteria.png';
+import acougue from '../assets/estabImagens/acougue.png';
+import churrascaria from '../assets/estabImagens/churrascaria.png';
+import confeitaria from '../assets/estabImagens/confeitaria.png';
+import hamburgueria from '../assets/estabImagens/hamburgueria.png';
+import japones from '../assets/estabImagens/japones.png';
+import mercado from '../assets/estabImagens/mercado.png';
+import posto from '../assets/estabImagens/posto.png';
+
 
 const imagemCategoria = {
   'Padaria': padaria,
@@ -11,6 +20,14 @@ const imagemCategoria = {
   'Café': cafe,
   'Bar': bar,
   'Pizzaria': pizzaria,
+  'Sorveteria': sorveteria,
+  'Açougue': acougue,
+  'Churrascaria': churrascaria,
+  'Confeitaria': confeitaria,
+  'Hamburgueria': hamburgueria,
+  'Sushi': japones,
+  'Mercado': mercado,
+  'Posto de Combustível': posto,
 };
 
 export const getEstabelecimentoImagem = (categoria) => {
