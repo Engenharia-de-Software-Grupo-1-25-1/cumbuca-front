b86988b057f1931f392355947a68ed3cec94510b
lucsa-a <lucas.andre.monteiro.sousa@ccc.ufcg.edu.br>
2025-08-22 13:20:02 -0300

    refactor: altera ícone de TagBox

--- src/components/layouts/TagBox.jsx
+++ src/components/layouts/TagBox.jsx
@@ -1,4 +1,4 @@
-import { FiTag } from 'react-icons/fi';
+import { FaHashtag } from "react-icons/fa6";
 
 //Box de tag popular, cada tag apresenta um ícone, seu nome e seu número de publicações
 //As cores do componente são definidas pelos parâmetros corFundo e corDestaque
@@ -13,7 +13,7 @@ function TagBox({ tag, corFundo, corDestaque }) {
         borderColor: corDestaque,
       }}
     >
-      <FiTag color={corDestaque} size="30" />
+      <FaHashtag color={corDestaque} size="30" />
       <p className="text-[#1e1e1e] font-medium text-[18px]">{tag.tag}</p>
       <p
         className="text-[#fefefe] rounded-xl px-2.5 py-0.5 text-[18px] ml-auto sm:ml-0 md:ml-0 lg:ml-auto sm:text-center"
