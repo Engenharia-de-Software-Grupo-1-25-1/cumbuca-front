81b08369c4093896560a419be7c980ecd936b532
lucsa-a <lucas.andre.monteiro.sousa@ccc.ufcg.edu.br>
2025-08-26 22:58:37 -0300

    fix: remove shrink de lupa de pesquisar

--- src/components/headers/BarraDePesquisa.jsx
+++ src/components/headers/BarraDePesquisa.jsx
@@ -16,7 +16,7 @@ function BarraDePesquisa({ placeholder = 'Pesquisar...' }) {
 
   return (
     <div className="flex items-center bg-[#f5dfb6] border border-[#db520a] rounded-full sm:px-0 md:px-4 lg:px-4 py-2 w-[45%] sm:w-[45%] md:w-[80%] lg:w-full max-w-4xl">
-      <IoSearch alt="Pesquisar" color="#DB520A" className="w-[40px] h-[40px] hidden sm:block ml-4" />
+      <IoSearch alt="Pesquisar" color="#DB520A" className="w-[40px] h-[40px] hidden sm:block ml-4 flex-shrink-0" />
       <input
         type="search"
         ref={inputRef}
