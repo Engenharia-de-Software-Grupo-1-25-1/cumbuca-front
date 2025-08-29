d65dc0dca1d523eec3e212edf5e4c55bcd4b72d5
KaioCar64 <kaio.silva.carvalho@ccc.ufcg.edu.br>
2025-08-28 23:02:32 -0300

    fix: remove filtros e pesquisa da página de estabelecimento

---
+++ src/components/layouts/Layout6.jsx
@@ -0,0 +1,23 @@
+import Header from '../headers/Header3';
+import NavBar from './NavBar';
+import TagsPopulares from './TagsPopulares';
+
+//Layout Padrão com Tags Populares e Header sem pesquisa e filtros
+//É possível definir o conteúdo que do lado esquerdo do Layout utilizando o parâmetro Conteudo
+//Atributos de Conteudo são definidos em conteudoProps
+function Layout({ Conteudo, conteudoProps }) {
+  return (
+    <>
+      <Header />
+      <div className="max-w-6xl mx-auto px-0 sm:px-0 md:px-4 lg:px-4">
+        <NavBar />
+        <div className="flex gap-4 justify-center ml-0 sm:ml-0 md:ml-4 lg:ml-4 mb-8">
+          {Conteudo ? <Conteudo {...conteudoProps} /> : null}
+          <TagsPopulares />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Layout;
