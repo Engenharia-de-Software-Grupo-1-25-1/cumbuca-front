8109d396ff6d0b9344038ea9a60301d5eb6ca0a7
KaioCar64 <kaio.silva.carvalho@ccc.ufcg.edu.br>
2025-08-29 18:21:24 -0300

    feat: filtro de estabelecimento

--- src/pages/FeedEstabelecimentos.jsx
+++ src/pages/FeedEstabelecimentos.jsx
@@ -16,7 +16,7 @@ function Layout() {
 
   return (
     <>
-      <Header onAplicarFiltros={handleAplicarFiltros} />
+      <Header onAplicarFiltros={handleAplicarFiltros} filtros={filtros} ordenador={ordenador} />
       <div className="max-w-6xl mx-auto px-0 sm:px-0 md:px-4 lg:px-4">
         <NavBar />
         <div className="flex gap-4 justify-center ml-0 sm:ml-0 md:ml-4 lg:ml-4 mb-8 no-scrollbar overflow-y-scroll max-h-[calc(100vh-250px)]">
