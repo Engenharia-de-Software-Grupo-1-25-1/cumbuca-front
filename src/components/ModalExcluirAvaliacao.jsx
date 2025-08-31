0d3db5db9abeb8f5c5d1b5cb251d4902664b5387
MFernandaRicarte <maria.fernanda.fernandes.ricarte@ccc.ufcg.edu.br>
2025-08-30 22:51:27 -0300

    chore: remove avaliação + mudança design modal

--- src/components/ModalExcluirAvaliacao.jsx
+++ src/components/ModalExcluirAvaliacao.jsx
@@ -1,3 +1,5 @@
+import { FaExclamationTriangle } from 'react-icons/fa';
+
 export default function ModalExcluirAvaliacao({
   open,
   title = 'Excluir Avaliação',
@@ -10,12 +12,9 @@ export default function ModalExcluirAvaliacao({
 
   return (
     <div className="fixed inset-0 z-[1000]">
-      {/* backdrop */}
       <div className="absolute inset-0 bg-black/50" onClick={onCancel} />
 
-      {/* modal */}
       <div className="absolute left-1/2 top-1/2 w-[320px] -translate-x-1/2 -translate-y-1/2 rounded-xl border-2 border-[#d8a35b] bg-[#f4e1b6] p-5 shadow-xl">
-        {/* close */}
         <button
           onClick={onCancel}
           className="absolute right-3 top-3 text-[#7a4a1f] text-xl leading-none"
@@ -24,9 +23,8 @@ export default function ModalExcluirAvaliacao({
           ×
         </button>
 
-        {/* ícone (alerta) */}
         <div className="mx-auto mb-2 flex h-12 w-12 items-center justify-center rounded-lg bg-[#f9c56d]">
-          <span className="text-2xl text-[#a63b1f]">!</span>
+          <FaExclamationTriangle className="text-[#a63b1f]" size={28} />
         </div>
 
         <h2 className="mb-1 text-center text-xl font-extrabold text-[#1e1e1e]">{title}</h2>
