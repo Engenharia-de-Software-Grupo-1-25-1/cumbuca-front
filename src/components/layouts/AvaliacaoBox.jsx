03bc1f67606110aa6ace68c9ab51760a5ab7b097
lucsa-a <lucas.andre.monteiro.sousa@ccc.ufcg.edu.br>
2025-09-05 11:56:24 -0300

    refactor: remove height duplicado

--- src/components/layouts/AvaliacaoBox.jsx
+++ src/components/layouts/AvaliacaoBox.jsx
@@ -95,7 +95,7 @@ export default function AvalicaoBox({ avaliacao, onChange, onSelecionarTag }) {
                     : fotoDePerfilPadrao
                   : fotoDePerfilPadrao
               }
-              className="rounded-full hover:brightness-90 transition duration-300 h-[48px] md:h-[54px] lg:h-[65px] h-[48px] md:w-[54px] lg:w-[65px] object-cover"
+              className="rounded-full hover:brightness-90 transition duration-300 h-[48px] md:h-[54px] lg:h-[65px] w-[48px] md:w-[54px] lg:w-[65px] object-cover"
               alt={`Foto de perfil de ${avaliacao.usuario.nome}`}
             />
           </Link>
